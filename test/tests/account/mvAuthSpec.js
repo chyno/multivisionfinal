a35c4127539a242d0591eaa39312b1cfc77ddf3f
John Chynoweth <JChynoweth@vmdsystems.com>
2014-05-12 13:44:21 -0400

    mend
    test project

---
+++ test/tests/account/mvAuthSpec.js
@@ -0,0 +1,29 @@
+﻿describe( 'mvAuth', function () {
+
+    beforeEach( module( 'app' ) );
+    beforeEach( inject( function ( $window ) {
+        $window.bootstrappedUserObject = { username: 'before', roles: ['admin'] };
+    }) );
+
+
+    describe( 'canCreateNewUser', function () {
+        it('Test mvAuth can create a new user', inject(function(mvAuth, mvIdentity) {
+
+                var newUserData = {
+                    username: 'jchynwoeth',
+                    password: 'pswrd',
+                    firstName: 'John',
+                    lastName: 'Chynoweth'
+                };
+
+                mvAuth.createUser(newUserData).then(function(data) {
+                    expect(data).to.be.falsy;
+                });
+
+            })
+        );
+    });
+
+
+
+});
---
+++ test/tests/account/mvIdentitySpec.js
@@ -0,0 +1,23 @@
+﻿describe('mvIdentity', function() {
+     
+    beforeEach(module('app')); 
+
+
+    beforeEach(inject(function($window) {
+        $window.bootstrappedUserObject = {username:'jchynwoeth', roles: ['admin']};
+    }));
+
+
+
+    describe('isAuthenicatedUser', function() {
+        it('should be a valid Identity object', inject(function(mvIdentity) {
+           expect(mvIdentity.isAuthenticated()).to.be.true;
+        }));
+    });
+
+     describe('isAuthenicatedUser', function() {
+        it('should be a valid Identity object', inject(function(mvIdentity) {
+           expect(mvIdentity.isAuthorized('admin')).to.be.true;
+        }));
+    });
+})
