1d122de583f9bcbf857f375f41d5fc38832d0e2c
John Chynoweth <JChynoweth@vmdsystems.com>
2014-05-14 14:16:57 -0400

    complete asynch testing fro create useer

--- test/tests/account/mvAuthSpec.js
+++ test/tests/account/mvAuthSpec.js
@@ -1,29 +1,39 @@
 ﻿describe( 'mvAuth', function () {
+    var mvA, mvI;
 
     beforeEach( module( 'app' ) );
+    
     beforeEach( inject( function ( $window ) {
-        $window.bootstrappedUserObject = { username: 'before', roles: ['admin'] };
+        $window.bootstrappedUserObject = {password: 'pswrdxx', firstName: 'Johnxx', lastName: 'Chynowethxx', username: 'before', roles: ['xx'] };
+        
     }) );
 
+     beforeEach(inject(function(mvAuth, mvIdentity) {
 
-    describe( 'canCreateNewUser', function () {
-        it('Test mvAuth can create a new user', inject(function(mvAuth, mvIdentity) {
+         mvA = mvAuth;
+         mvI = mvIdentity;
+         
 
-                var newUserData = {
-                    username: 'jchynwoeth',
-                    password: 'pswrd',
-                    firstName: 'John',
-                    lastName: 'Chynoweth'
-                };
+          var newUserData = {
+                username: 'jchynwoeth',
+                password: 'pswrd',
+                firstName: 'John',
+                lastName: 'Chynoweth'
+            };
+         mvA.createUser(newUserData);
+        
 
-                mvAuth.createUser(newUserData).then(function(data) {
-                    expect(data).to.be.falsy;
-                });
+     }) );
 
-            })
-        );
+
+    it('should be able to add user', function(done) {
+        
+        done();
+        expect(mvI.currentUser.username).equal("jchynwoeth");
+        
     });
 
 
-
 });
+
+
